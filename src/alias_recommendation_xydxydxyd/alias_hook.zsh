9c1a21db1f7f9ad38c40ebb9bea58619ce9b7738
Eric Xu <xydxydxyd1@gmail.com>
2024-06-30 14:26:28 -0700

    Get alias keys and values

--- src/alias_recommendation_xydxydxyd/alias_hook.zsh
+++ src/alias_recommendation_xydxydxyd/alias_hook.zsh
@@ -1,12 +1,34 @@
 # preexec ZSH hook for suggesting aliases
 
-rm ./suggest_real_time.log
+#rm ./suggest_real_time.log
+
+local input_matcher="(^[^=]+)=(.+)$"
+
+
+get_alias_keys() {
+    alias_keys=""
+    for k in "${(@k)aliases}"; do
+        alias_keys="$alias_keys\n$k"
+    done
+    echo $alias_keys
+}
+
+
+get_alias_vals() {
+    alias_vals=""
+    for k in "${(@k)aliases}"; do
+        alias_vals="$alias_vals\n${aliases[$k]}"
+    done
+    echo "$alias_vals"
+    echo $alias_vals
+}
+
 
 suggest_alias() {
     local suggest_cmd='python3 ./suggest_real_time.py "$(history "-10" | cut -c 8-)" "$(alias)" --ignored_cmds "$IGNORED_ALIAS" --min_rating 25'
     local suggested_alias_cmd=$(eval $suggest_cmd)
-    local matcher="^alias ([a-zA-Z0-9_]+)=\\$'(.*)'$"
-    if [[ "$suggested_alias_cmd" =~ $matcher ]]; then
+    local output_matcher="^alias ([a-zA-Z0-9_]+)=\\$'(.*)'$"
+    if [[ "$suggested_alias_cmd" =~ $output_matcher ]]; then
         local alias_name="${match[1]}"
         local alias_value="${match[2]}"
         echo "Suggested alias: $alias_name='$alias_value'"
